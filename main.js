4bac93e188c7fc1f45871f7bb34d8b42dc4ff0e8
danelzhan <164102172+danelzhan@users.noreply.github.com>
2024-08-01 18:13:36 +0800

    tried smth else

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 //import 'style.css';
 
-import * as THREE from 'three';
+import * as THREE from './node_modules/three/build/three.module.js';
 
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { GLTFLoader } from "./node_modules/three/examples/jsm/loaders/GLTFLoader.js";
 
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
 
 // scene setup
 
